d0e38829f2ede34f95f1972ed6a2a947980b9d49
Lowellll <yejin2651@naver.com>
2018-07-29 23:02:03 +0900

    지역별 미세먼지 확인 기능 구현 (리스트화)
    측정소명으로 미세먼지를 확인하던 기능을 지역별로 교체.

--- routes/finedust.js
+++ routes/finedust.js
@@ -1,23 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-router.post('/city',function(req,res){
+var parseString = require('xml2js').parseString; // xml parsing -> json
+var inspect = require('util').inspect;
+
+router.post('/city',function(req,res){ // 지역별로 도시의 미세먼지 확인
   const ServiceKey = "%2Fi9G3u9IiKGAUwHWqz%2FHganxLxlIopwPFbXkIBufTEeCEYL3MyEmUPxm9f42AkbI%2Fp6QH5hZBQC%2BJLagWGm3%2Bw%3D%3D";
   var body = req.body;
   var city = body.city;
-  var request_url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+  var request_url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst";
 
   var queryParams = '?' + encodeURIComponent('ServiceKey') + `=${ServiceKey}`; /* Service Key*/
-  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* 한 페이지 결과 수 */
+  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('50'); /* 한 페이지 결과 수 */
   queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지 번호 */
-  queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent(city); /* 시도 이름 (서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종) */
-  queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.3'); /* 버전별 상세 결과 참고문서 참조 */
+  queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent(city); /* 도시 이름 */
+  queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComponent('HOUR');
   
   request({
     url: request_url + queryParams,
     method: 'GET'
   }, function (error, response, body) {
-    res.render('finedust_list',{'city':city,'list':body});
+    parseString(body, function(err,result){ // xml parsing
+      var finedust_info = result.response.body[0].items[0].item;
+      var context = {};
+      
+      context['city'] = city;
+      context['items'] = finedust_info;
+
+      // console.log(result.response.body[0].totalCount);
+      res.render('finedust_list',context);
+    });
   });
 });
 
