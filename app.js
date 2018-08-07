dd16b649d2e2c4d4698338725caae82a1a22274a
Lowellll <yejin2651@naver.com>
2018-08-07 13:42:12 +0900

    router 로직 -> middleware 로직 변경
    리팩토링?_?
    라우터에 있는 검색어 자동완성 로직은 깔끔하지 못하다고 생각해 app.js에 middleware로 옮겼다.
    이게 맞는건강..?

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var express =  require('express')
 , server = http.createServer(app)
 , indexRouter = require('./routes/index')
 , finedustRouter = require('./routes/finedust')
-, bodyParser = require('body-parser');
+, bodyParser = require('body-parser')
+,fs = require('fs');
+
 
 // bodyparser middleware 사용
 app.use(bodyParser.urlencoded({extended:false}));
@@ -13,6 +15,29 @@ app.use(express.static('public')); // static file serving
 
 app.set('view engine','jade'); // template 
 app.set('views','./views');
+
+// 자동완성 변수를 위한 middleware 생성
+app.get('/',function(request,response,next){
+    var json = fs.readFileSync('./korea-administrative-district.json');
+    var temp_datas = JSON.parse(json);
+    var datas = temp_datas.data
+
+    // 자동완성을 위한 배열 생성
+    var district = [];
+    for(var i=0; i<datas.length;i++){ // 지역 (서울,부산) 배열 생성(value 검색하는 데 사용)
+        Array.prototype.push.apply(district,Object.keys(datas[i])); // apply ['123'] + ['34'] = ['123','34'] 배열 하나로 합치기 (concat과 살짝 다름)
+    }
+
+    var district_arr = [];
+    for(var i=0; i<datas.length;i++){
+        for(var j = 0; j<Object.keys(datas[i][district[i]]).length;j++){ 
+            district_arr.push(district[i]+" "+datas[i][district[i]][j]);  // 배열 요소 추가
+        }
+    };
+
+    request.district = district_arr;
+    next(); // 다음 미들웨어를 호출
+});
 
 app.use('/',indexRouter);
 app.use('/finedust',finedustRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,8 @@
 const express = require('express');
 var router = express.Router();
-var fs = require('fs');
 
 router.get('/',function(req,res){
-    var json = fs.readFileSync('./korea-administrative-district.json');
-    var temp_datas = JSON.parse(json);
-    var datas = temp_datas.data
-
-    // 자동완성을 위한 배열 생성
-    var district = [];
-    for(var i=0; i<datas.length;i++){ // 지역 (서울,부산) 배열 생성(value 검색하는 데 사용)
-        Array.prototype.push.apply(district,Object.keys(datas[i])); // apply ['123'] + ['34'] = ['123','34'] 배열 하나로 합치기 (concat과 살짝 다름)
-    }
-
-    var district_arr = [];
-    for(var i=0; i<datas.length;i++){
-        for(var j = 0; j<Object.keys(datas[i][district[i]]).length;j++){ 
-            district_arr.push(district[i]+" "+datas[i][district[i]][j]);  // 배열 요소 추가
-    }}
-
-    res.render('index',{data:district_arr});
+    res.render('index',{data:req.district});
 })
 
 module.exports = router;
