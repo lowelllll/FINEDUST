2481d512105c4cd799680072612998a08e1f5b54
Lowellll <32219612+lowelllll@users.noreply.github.com>
2018-08-20 23:53:21 +0900

    다국어 지원 처리 - index page
    cookie를 이용해 다국어 지원 처리 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
-const express = require('express');
-var router = express.Router();
+const express = require('express')
+,router = express.Router()
+,cookie = require('cookie');
 
 router.get('/',function(req,res){
-    res.render('index',{data:req.district});
+    var language = ""; // language 쿠키
+
+    if (!req.headers.cookie) { // 처음 접속해서 쿠키가 없는 경우.
+        res.set({'Set-Cookie':[
+            `language=KR;`
+        ]});
+        language = "KR";
+    } else {
+        var headerCookie = req.headers.cookie;
+        var cookies = cookie.parse(headerCookie);
+        language = cookies.language;
+    }
+
+    res.render('index',{data:req.district,language:language});
 })
 
 module.exports = router;
